1450b05c2705e0d4012af00d661e512bdedc79a3
Georgi Mihov <gmihov001@gmail.com>
2022-02-06 11:17:22 -0500

    Updates

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import Main from "./Main";
 import GenerateQR from "./GenerateQR";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Main } from "./Main";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
