4f6c13d77809f4819fc43860574fe51ec8247459
Georgi Mihov <gmihov001@gmail.com>
2022-02-07 12:25:12 -0500

    Updates

--- GenerateQR.js
+++ GenerateQR.js
@@ -9,28 +9,14 @@ function SimpleQR() {
 export function GenerateQR() {
   const uri =
     "https://3000-4geeksacademy-flaskresth-t8qn0i28cw9.ws-us30.gitpod.io/reading";
-  const [user, setUser] = React.useState("Georgi Mihov");
+  const [user, setUser] = React.useState("Claimant User");
   const [coords, setCoords] = React.useState("12345.6789");
+  const [data, setData] = React.useState({ claimant: user, coords: coords });
+  const dataJson = JSON.stringify(data);
 
   return (
     <View style={styles.container}>
-      {/* <TouchableOpacity
-        onPress={() => alert("Hello, world!")}
-        style={{
-          backgroundColor: "royalblue",
-          borderRadius: "15px",
-          width: "90%",
-          margin: "5%",
-          padding: "5%",
-          display: "flex",
-          justifyContents: "center",
-        }}
-      >
-        <Text style={{ fontSize: 20, color: "#fff", textAlign: "center" }}>
-          GENERATE QR CODE
-        </Text>
-      </TouchableOpacity> */}
-      <SvgQRCode size={180} value={`${uri}?gps=${coords}&username=${user}`} />
+      <SvgQRCode size={180} value={dataJson} />
     </View>
   );
 }
--- ReadQR.js
+++ ReadQR.js
@@ -4,26 +4,55 @@ import { BarCodeScanner } from "expo-barcode-scanner";
 
 export function ReadQR() {
   const [hasPermission, setHasPermission] = useState(null);
-  const [svanned, setScanned] = useState(false);
+  const [scanned, setScanned] = useState(false);
+  const [thisUser, setThisUser] = useState("Proving User");
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    let dataJson = JSON.parse(data);
+
+    alert(`Bar code data ${dataJson.claimant} scanned`);
+    console.log(dataJson.claimant);
+    setScanned(true);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => alert("Hello, world!")}
-        style={{
-          backgroundColor: "royalblue",
-          borderRadius: "15px",
-          width: "90%",
-          margin: "5%",
-          padding: "5%",
-          display: "flex",
-          justifyContents: "center",
-        }}
-      >
-        <Text style={{ fontSize: 20, color: "#fff", textAlign: "center" }}>
-          READ QR CODE
-        </Text>
-      </TouchableOpacity>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && (
+        <TouchableOpacity
+          onPress={() => setScanned(false)}
+          style={{
+            backgroundColor: "royalblue",
+            borderRadius: "15px",
+            width: "90%",
+            margin: "5%",
+            padding: "5%",
+            display: "flex",
+            justifyContents: "center",
+          }}
+        >
+          <Text style={{ fontSize: 20, color: "#fff", textAlign: "center" }}>
+            Tap to Scan Again
+          </Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
